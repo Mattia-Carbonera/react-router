2ca0bcf1f4379aa77a7c1ec65ca01810b6f03c71
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-12-13 14:50:35 +0100

    add code

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// import layout
 import DefaultLayout from "./components/DefaultLayout/DefaultLayout";
+
+// import page
 import HomePage from "./components/HomePage";
 import AboutUs from "./components/AboutUs";
 import MyPosts from "./components/MyPosts";
